67ca277c7da2c95c7d3054333192cba07fb4e086
Akash Mitra <akashmitra@gmail.com>
2017-05-26 00:04:11 +0800

    initial release

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,162 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- <link rel="icon" href="../../favicon.ico"> -->
+
+    <title>Different Half</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="https://maxcdn.bootstrapcdn.com/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">
+              <img src="/img/logo-small.png" class="logo" />
+          </a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <!-- <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul> -->
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="/">Home</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+
+    <div class="container monospace">
+      <a name="home"></a>
+      <div class="row">
+          <div class="col-sm-5">
+            <p>&nbsp;</p>
+            <p>&nbsp;</p>
+            <h1>Creative Engineering for Humanity...</h1>
+            <p>&nbsp;</p>
+            <p>
+              In differentHalf, we are building space for engineering creativity. 
+              We believe that human ingenuity and innovation can not be managed. 
+              Nor we manage our company in any conventional way.
+            </p>    
+            <p>
+                We embrace uncertainty. We celebrate nerdiness. And we solve problems.
+            </p>
+
+          </div>
+          <div class="col-sm-1">
+            <p>&nbsp;</p>
+          </div>
+          <div class="col-sm-5">
+            <img src="/img/rendered.png" class="front-logo">
+          </div>
+      </div>
+
+      <hr>
+      <a name="projects"></a>
+      <div class="row">
+        <h1 class="homepage-heading">Projects</h1>
+          <div class="col-sm-4">
+              <div class="slate">
+                  <h3>TensorBolt</h3>
+                  <p>
+                      Machine learning for the business
+                  </p>
+              </div>
+          </div>
+
+
+          <div class="col-sm-4">
+              <div class="slate">
+                  <h3>HealthLog</h3>
+                  <p>
+                      Personal health information &amp; analytics
+                  </p>
+              </div>
+          </div>
+
+          <div class="col-sm-4">
+              <div class="slate">
+                  <h3>Casts</h3>
+                  <p>
+                      Specialized educational web-casts
+                  </p>
+              </div>
+          </div>
+      </div>
+
+      
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <hr>
+      <a name="contact"></a>
+      <div class="row">
+          <div class="col-md-12">
+              <h1 class='homepage-heading'>Contact</h1>
+              <p style="text-align: center; margin-top: -70px">
+                If you are the smartest kid in the town, and if you are looking for flexible working options, reach out to us at 
+                <br>
+                  admin [at] differentHalf [dot] com
+              </p>
+          </div>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
